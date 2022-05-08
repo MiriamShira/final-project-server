d9009d559a9f8cafeadfe940f29446e765ebe9a8
Miriam <mreichman309@gmail.com>
2022-05-08 23:25:26 +0300

    hashing password

--- Controller/user.js
+++ Controller/user.js
@@ -1,13 +1,20 @@
 const userModel = require('../Model/user');
 //const { ObjectId } = require('mongodb');
 // const logger = require('../log/logger');
-
+const bcrypt=require('bcrypt');
 module.exports.signUp = async function (req, res, next) {
     try {
         const { firstname, lastname, language, email,password } = req.body;
-        const data = new userModel({firstname:firstname, lastname:lastname, language:language, email:email,password:password});
+       bcrypt.hash(password,10,(error,hash)=>{
+           if(error){
+               //res.status(500).json({error})
+               next(error)
+           }
+            const data = new userModel({firstname:firstname, lastname:lastname, language:language, email:email,password:hash});
         const insertUser = await data.save();
         await res.send(insertUser);
+       })
+       
     }
     catch (e) {
         next(e);
@@ -17,11 +24,20 @@ module.exports.login= async function (req, res, next) {
     try {
         const userName = req.params.userName;
         const password = req.params.password;
-        const getUser = await userModel.findOne({ email: userName, password: password });
-        if (getUser)
-            res.send(getUser);
+        const getUser = await userModel.findOne({ email: userName});
+        if (getUser){
+            bcrypt.compare(password,getUser.password,(error,result)=>{
+               if(error) {
+                next(error);   
+               }
+               res.send(getUser);
+               result
+            })
+            res.status(401).send('something whent worng.....')
+        }
+            
         else
-            res.status(204).send(getUser)
+            res.status(204).send('something whent worng.....')
     }
     catch (e) {
         next(e);
