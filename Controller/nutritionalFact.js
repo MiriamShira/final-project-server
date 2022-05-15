efaec1c1ca4326526716f92f935413162e392fe9
ruthshira <212724256@mby.co.il>
2022-05-15 21:19:03 +0300

    The most terible version

--- Controller/nutritionalFact.js
+++ Controller/nutritionalFact.js
@@ -1,5 +1,6 @@
 const NutritionalFactModel =require('../Model/NutritionalFact');
 const mongoose=require('mongoose');
+
 module.exports.getNutritionalFact=async function (req,res,next){
     try{
         const NutritionalFact=await NutritionalFactModel.find();
